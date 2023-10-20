8d8ea22b3e3beb4fae5a1d193576b697922453a3
LuizKramer <luizeduardockramer@gmail.com>
2023-10-20 10:39:00 -0300

    feat(app.js): Add insertOne method

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const uri = process.env.MONGO_URI;
 const dbName = process.env.MONGO_DBNAME;
 const client = new MongoClient(uri);
 
+const accountsCollection = client.db(dbName).collection('accounts');
+
 
 const connectToDatabase = async () => {
     try {
@@ -16,9 +18,19 @@ const connectToDatabase = async () => {
     }
 }
 
+const sampleAccount = {
+    account_holder: "John Doe",
+    account_id: "123456789",
+    account_type: "Checking",
+    balance: 1000,
+    last_updated: new Date()
+}
+
 const main = async () => {
     try {
         await connectToDatabase();
+        let result = await accountsCollection.insertOne(sampleAccount);
+        console.log(`Inserted with the following id(s): ${result.insertedId}`);
     } catch (error) {
         console.error(`Error connecting to the ${dbName} MongoDB database! ${error}`);
     }
